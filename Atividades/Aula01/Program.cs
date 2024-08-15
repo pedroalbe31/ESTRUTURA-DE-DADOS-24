287590e7f72a75b7d825d49a47756275d02cdd2e
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-08-14 22:07:34 -0300

    Primeiro projeto .net

---
+++ Atividades/Aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿// comentario de uma linha
+
+/*
+comentario de 
+multiplas linhas 
+*/
+
+// Declarando variavel
+int number;
+// atribuindo valor
+number = 10;
+
+// declarando constante 
+const int DAYS_IN_WEEK = 7;
+
+// imprimindo string contatenada
+Console.WriteLine($"A Semana tem {DAYS_IN_WEEK} dias.");
