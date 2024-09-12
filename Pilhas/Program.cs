9859b79c63877dcd58b2b3507b9caf1478b1ee27
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-09-11 21:51:42 -0300

    Pilhas

---
+++ Pilhas/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Basicamente as pilhas trabalham com uma estrutura LIFO -> Last In First Out
+Três métodos Importantes:
+Push() -> Insere um elemento no topo da pilha
+Pop() -> Insere um elemento do topo e o retorna
+Peek() -> Retorna o elemento do topo sem removêlo
+
+Invertendo as bolas
+*/
+Stack<char> caracteres = new Stack<char>();
+foreach (char c in "DARCY DA MONTANHA")
+{
+    caracteres.Push(c); 
+}
+
+//DESEMPILHANDO
+string Invertido = string.Empty;
+while(caracteres.Count > 0)
+{
+    Invertido += caracteres.Pop();
+}
+
+Console.WriteLine(Invertido);
