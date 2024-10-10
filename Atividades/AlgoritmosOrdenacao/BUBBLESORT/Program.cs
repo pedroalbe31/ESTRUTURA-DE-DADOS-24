6212148a9f147e4398d6f04fd97a50eca14c2bc9
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-10-09 22:00:20 -0300

    AlgoritimosdeOrdenacao

---
+++ Atividades/AlgoritmosOrdenacao/BUBBLESORT/Program.cs
@@ -0,0 +1,17 @@
+﻿using BubbleSort;
+
+int [] Inteiros = [0, 12, 7, 1, 2, 8];
+
+BubbleSort.BubbleSort.Print(Inteiros);
+
+BubbleSort.BubbleSort.Sort<int>(Inteiros);
+
+BubbleSort.BubbleSort.Print(Inteiros);
+
+Console.WriteLine("Teste Char");
+
+char [] chars = {'x', 'z', 'c', 'a'};
+
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort(chars);
+BubbleSort.BubbleSort.Print(chars);
---
+++ Atividades/AlgoritmosOrdenacao/InsertionSort/Program.cs
@@ -0,0 +1,15 @@
+﻿using InsertionSort;
+
+int [] Inteiros = [0, 12, 7, 1, 2, 8];
+
+InsertionSort.InsertionSort.Print(Inteiros);
+InsertionSort.InsertionSort.Sort(Inteiros);
+InsertionSort.InsertionSort.Print(Inteiros);
+
+Console.WriteLine("Teste Char");
+
+char [] chars = {'x', 'z', 'c', 'a'};
+
+InsertionSort.InsertionSort.Print(chars);
+InsertionSort.InsertionSort.Sort(chars);
+InsertionSort.InsertionSort.Print(chars);
