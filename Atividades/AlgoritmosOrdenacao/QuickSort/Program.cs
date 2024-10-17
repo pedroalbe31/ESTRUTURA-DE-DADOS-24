e816bf3c9f7f299dcdb81b7b8ba7e5c2bf087be6
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-10-16 21:32:17 -0300

    QuickSort

---
+++ Atividades/AlgoritmosOrdenacao/QuickSort/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Testando o Quick Sort");
+
+int [] values = new int [] {0, 15, 24, 1, 3, 6, 12, 9};
+
+QuickSort.QuickSort.Print<int>(values);
+Console.WriteLine("Iniciando Ordenação: ");
+QuickSort.QuickSort.Sort<int>(values, 0, values.Length - 1);
+Console.WriteLine("Finalizando odenação: ");
+QuickSort.QuickSort.Print<int> (values);
