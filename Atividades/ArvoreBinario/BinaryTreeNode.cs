45d10b15180e0c7b0ec665291205d2ef90ae19ee
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-11-06 21:13:54 -0300

    ArvoreBinaria

---
+++ Atividades/ArvoreBinario/BinaryTreeNode.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArvoreBinario
+{
+    public class BinaryTreeNode<T> : TreeNode<T>
+    {
+        public BinaryTreeNode()
+        {
+            base.Children = new List<TreeNode<T>>();
+        }
+        public BinaryTreeNode<T> Left
+        {
+            get { return (BinaryTreeNode<T>)Children[0];} //<-- CONVERSÃO DE TIPO {CAST}
+            set { Children[0] = value; }
+        }
+        public BinaryTreeNode<T> Right
+        {
+            get { return (BinaryTreeNode<T>)Children[1];}
+            set { Children[1] = value; }
+        }
+    }
+}
