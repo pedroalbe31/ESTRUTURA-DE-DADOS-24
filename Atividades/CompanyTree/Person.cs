48e7d21fbab39c3c4d85b2ffee585fc5677e7c25
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-11-27 20:07:05 -0300

    CompanyTree

---
+++ Atividades/CompanyTree/Person.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyStructure
+{
+    public class Person
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Role { get; set; }
+        public Person() { }
+        public Person(int id, string name, string role)
+        {
+            Id = id;
+            Name = name;
+            Role = role;
+
+        }
+    }
+}
