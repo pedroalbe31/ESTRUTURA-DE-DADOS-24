c151a11ab7b295791af9f05faf2b8bb7b434fd59
Pedro Henrique Albuquerque <428915@unoesc.lan>
2024-11-27 20:18:21 -0300

    HashTable

---
+++ HashTable/Program.cs
@@ -0,0 +1,36 @@
+﻿//Secret hashmap
+
+using System.Collections;
+using System.Linq.Expressions;
+
+Hashtable secret = new()
+{
+	{ "Vinicius", "Urso pardo" },
+	{ "Gabriel", "Veado" },
+	{ "Mauricio", "Gorila" },
+	{ "Thiago", "Jacaré" },
+	{ "Guilherme", "Cabra" },
+	{ "Gustavo", "Canguru" }
+};
+
+secret.Add("Daniel", "Coelho");
+secret["Igor"] = "Rato";
+
+try
+{
+	secret.Add("Vinicius", "Ursão");
+} 
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao tentar incluir valor repetido.");
+	Console.WriteLine(ex.Message);
+}
+
+//Percorrendo itens do hashmap
+Console.WriteLine("Membros do secreto e seu animal");
+if (secret.Count == 0)
+    Console.WriteLine("Não existem membros");
+else
+    foreach (DictionaryEntry entry in secret)
+        Console.WriteLine($"{entry.Key}:{entry.Value}");
+    
